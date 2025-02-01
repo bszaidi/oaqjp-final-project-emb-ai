2dd0665b098aabc7f84b6f066124033a4ec47b72
bszaidi <szaidi@block.xyz>
2025-02-01 04:51:24 -0500

    Add Error Handling in Emotion Detector and hoem route in server

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,7 +7,10 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests. post(url, json= myobj, headers= header )
     data = json.loads(response.text)
-
+    # Check for HTTP 400 status code (bad request)
+    if response.status_code == 400:
+        return {"sadness": None, "joy": None, "fear": None, "disgust": None, "anger": None, "dominant_emotion": None}
+    
     # Extract emotions and their scores
     emotions = data['emotionPredictions'][0]['emotion']
 
